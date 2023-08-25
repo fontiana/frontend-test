d1830fe031af27c637ad750d06ded851d1411258
Caio Haruo <caio.haruo@hotmail.com>
2023-08-25 15:40:42 -0300

    feature: Create symbol card skeleton

--- binance-app/src/components/SymbolCard/index.tsx
+++ binance-app/src/components/SymbolCard/index.tsx
@@ -3,29 +3,38 @@ import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check } from '@phosphor-icons/react';
 
 interface SymbolCardProps {
-  type?: 'header' | 'default';
+  type?: 'header' | 'default' | 'skeleton';
+  symbol?: string;
 }
 
-export function SymbolCard({ type = 'default' }: SymbolCardProps) {
+export function SymbolCard({ type = 'default', symbol }: SymbolCardProps) {
   return (
     <div
-      className={classNames('p-4 flex items-center gap-8', {
+      className={classNames('p-4 flex items-center gap-8 h-[3.75rem]', {
         'bg-gray-200': type === 'header',
       })}
     >
-      <Checkbox.Root className="w-5 h-5 bg-white border border-gray-400 rounded">
-        <Checkbox.Indicator className="w-5 h-5 rounded flex items-center justify-center bg-teal-500">
-          <Check weight="bold" className="w-4 h-4 text-white" />
-        </Checkbox.Indicator>
-      </Checkbox.Root>
+      {type === 'skeleton' ? (
+        <div className="w-5 h-5 animate-pulse bg-gray-200" />
+      ) : (
+        <Checkbox.Root className="w-5 h-5 bg-white border border-gray-400 rounded">
+          <Checkbox.Indicator className="w-5 h-5 rounded flex items-center justify-center bg-teal-500">
+            <Check weight="bold" className="w-4 h-4 text-white" />
+          </Checkbox.Indicator>
+        </Checkbox.Root>
+      )}
 
-      <span
-        className={classNames('text-lg', {
-          'font-bold': type === 'header',
-        })}
-      >
-        Symbol
-      </span>
+      {type === 'skeleton' ? (
+        <div className="w-16 h-7 animate-pulse bg-gray-200" />
+      ) : (
+        <span
+          className={classNames('text-lg', {
+            'font-bold': type === 'header',
+          })}
+        >
+          {type === 'header' ? 'Symbol' : symbol}
+        </span>
+      )}
     </div>
   );
 }
