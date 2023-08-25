9cf9a59d8239f161895245088b0ab1128e389f36
Caio Haruo <caio.haruo@hotmail.com>
2023-08-25 15:47:28 -0300

    feature: Query symbol by search input (TO-DO: Improve Performance)

---
+++ binance-app/src/components/SymbolCollection/index.tsx
@@ -0,0 +1,24 @@
+import { useContext } from 'react';
+import { SymbolCard } from '../SymbolCard';
+import { SymbolContext } from '@/contexts/SymbolContext';
+
+interface SymbolCollectionProps {
+  symbols: { symbol: string }[];
+}
+
+export function SymbolCollection({ symbols }: SymbolCollectionProps) {
+  const { symbolsQuery } = useContext(SymbolContext);
+  const { isLoading } = symbolsQuery;
+
+  return (
+    <div className="border border-gray-200 overflow-y-auto">
+      <SymbolCard type="header" />
+
+      {isLoading
+        ? [...Array(10)].map(() => <SymbolCard type="skeleton" />)
+        : symbols.map(({ symbol }) => (
+            <SymbolCard key={symbol} symbol={symbol} />
+          ))}
+    </div>
+  );
+}
--- binance-app/src/components/SymbolList/index.tsx
+++ binance-app/src/components/SymbolList/index.tsx
@@ -1,33 +1,59 @@
-import { SymbolCard } from '@/components/SymbolCard';
+import { SymbolContext } from '@/contexts/SymbolContext';
 import { MagnifyingGlass } from '@phosphor-icons/react';
+import classNames from 'classnames';
+import { useContext, useEffect, useState } from 'react';
+import { SymbolCollection } from '../SymbolCollection';
 
 export function SymbolList() {
+  const [search, setSearch] = useState('');
+  const [filteredSymbols, setFilteredSymbols] = useState<{ symbol: string }[]>(
+    []
+  );
+
+  const { symbolsQuery } = useContext(SymbolContext);
+  const { data, isLoading } = symbolsQuery;
+
+  useEffect(() => {
+    if (data) {
+      const filteredResults = data.symbols.filter(({ symbol }) =>
+        symbol.toLowerCase().includes(search.toLowerCase())
+      );
+
+      setFilteredSymbols(filteredResults);
+    } else {
+      setFilteredSymbols([]);
+    }
+  }, [data, search]);
+
   return (
-    <aside className="flex flex-col justify-between max-w-sm px-2 py-8 shadow-xl h-full">
-      <header className="space-y-3">
-        <div className="relative group">
-          <input
-            type="text"
-            placeholder="Search"
-            className="border border-gray-200 rounded px-3 py-2 w-full"
-          />
-          <MagnifyingGlass
-            weight="bold"
-            size={22}
-            className="text-gray-400 absolute top-2.5 right-3.5 group-focus-within:invisible"
-          />
-        </div>
+    <aside className="border border-gray-200 rounded-lg flex flex-col gap-3 max-w-sm w-full px-2 py-8 shadow-xl h-full">
+      <div className="relative group">
+        <input
+          type="text"
+          placeholder="Search"
+          className="border border-gray-200 rounded px-3 py-2 w-full"
+          value={search}
+          onChange={({ target }) => setSearch(target.value)}
+        />
+        <MagnifyingGlass
+          weight="bold"
+          size={22}
+          className="text-gray-400 absolute top-2.5 right-3.5 group-focus-within:invisible"
+        />
+      </div>
 
-        <div className="border border-gray-200">
-          <SymbolCard type="header" />
-          <SymbolCard />
-          <SymbolCard />
-          <SymbolCard />
-          <SymbolCard />
-        </div>
-      </header>
+      <SymbolCollection symbols={filteredSymbols} />
 
-      <button className="text-xl text-white bg-teal-500 text-center w-full rounded py-3 hover:bg-teal-600 transition-colors">
+      <button
+        disabled={isLoading}
+        className={classNames(
+          'mt-auto text-xl text-white text-center w-full rounded py-3 transition-colors',
+          {
+            'cursor-not-allowed bg-teal-600': isLoading,
+            'bg-teal-500 hover:bg-teal-600': !isLoading,
+          }
+        )}
+      >
         Add to List
       </button>
     </aside>
