baf39a4a5c7b11fd53d4d63cbebf79cbff1b7908
tuliodev <resendetulio51@gmail.com>
2023-09-29 23:30:04 -0300

    feature: add global font-family Roboto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
+import './assets/styles/Global/index.css';
+
 function App() {
-  return (
-   <div>Hello World</div>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
