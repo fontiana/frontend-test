f4669a682536e16f1e50ce6e86a0c3c092518702
tuliodev <resendetulio51@gmail.com>
2023-09-30 20:53:34 -0300

    feature: implement SymbolContextProvider on App.js

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 import './assets/styles/Global/index.css';
 import Home from './pages/Home';
+import {SymbolContextProvider} from './contexts/SymbolContext';
 
 function App() {
-  return <Home />;
+  return (
+    <SymbolContextProvider>
+      <Home />
+    </SymbolContextProvider>
+  );
 }
 
 export default App;
