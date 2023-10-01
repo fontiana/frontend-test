83d1cf768af2f31c77cc58c91c1764e7e3d4c579
tuliodev <resendetulio51@gmail.com>
2023-10-01 17:54:01 -0300

    feature: implement handleSymbolCheck function

--- src/components/SymbolElement/index.tsx
+++ src/components/SymbolElement/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Container, SymbolText} from '../../assets/styles/SymbolElement';
 import {Checkbox} from 'antd';
+import {SymbolContext} from '../../contexts/SymbolContext';
 
 interface SymbolElementProps {
   symbol: string;
 }
 
 export default function SymbolElement({symbol}: SymbolElementProps) {
+  const {handleSymbolCheck} = useContext(SymbolContext);
+
+  const handleCheckboxChange = () => {
+    handleSymbolCheck(symbol);
+  };
+
   return (
     <Container>
-      <Checkbox>
+      <Checkbox onChange={handleCheckboxChange}>
         <SymbolText>{symbol}</SymbolText>
       </Checkbox>
     </Container>
